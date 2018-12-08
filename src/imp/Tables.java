ca1b7984b1e6d9544f118f4df35c159d42e25cb6
Kiran <kirantej.cse@gmail.com>
2018-12-08 17:45:42 +0530

    table_commit

---
+++ src/imp/Tables.java
@@ -0,0 +1,13 @@
+package imp;
+
+public class Tables {
+
+	public static void main(String[] args) {
+
+		for(int i=1;i<=20;i++){
+			for(int j=i;j<=20;j++){
+				System.out.println(i+" * "+j+" = "+i*j);
+			}
+		}
+	}
+}
